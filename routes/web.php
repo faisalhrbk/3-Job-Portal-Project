d9a6746b44ab9aea6be780b1089a11015240c193
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-06 17:24:49 +0500

    showd account register page created route and controller

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(HomeController::class)->group(function(){
-Route::get('/', 'index')->name('home');
-Route::get('contact', 'contact')->name('contact');
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/', 'index')->name('home');
+    Route::get('contact', 'contact')->name('contact');
+});
+
+Route::controller(AccountController::class)->group(function () {
+    Route::get('account/register', 'register')->name('account.register');
 });
