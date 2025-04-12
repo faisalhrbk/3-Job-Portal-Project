1ef20460cde8f8069f7e5df0509069594d78d734
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-13 02:22:40 +0500

    route('jobs'). '?category='. $category->id   todays works has been done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
     <section class="section-1 py-5">
         <div class="container">
             <div class="card border-0 p-5 shadow">
-                <form action="" method="GET">
+                <form action="{{ route('jobs') }}" method="GET">
                 <div class="row">
                     <div class="col-md-3 mb-sm-3 mb-lg-0 mb-3">
                         <input type="text" class="form-control" name="keyword" id="keyword" placeholder="Keywords">
@@ -39,7 +39,8 @@
 
                     <div class="col-md-3 mb-xs-3 mb-sm-3 mb-lg-0">
                         <div class="d-grid gap-2">
-                            <a href="jobs.html" class="btn btn-primary btn-block">Search</a>
+                            {{-- <a href="jobs.html" class="btn btn-primary btn-block">Search</a> --}}
+                            <button type="submit" class="btn btn-primary btn-block">Search</button>
                         </div>
 
                     </div>
@@ -58,7 +59,7 @@
                     @foreach ($categories as $category)
                         <div class="col-lg-4 col-xl-3 col-md-6">
                             <div class="single_catagory">
-                                <a href="jobs.html">
+                                <a href="{{ route('jobs'). '?category='. $category->id }}">
                                     <h4 class="pb-2">{{ $category->name }}</h4>
                                 </a>
                                 <p class="mb-0"> <span>0</span> Available position</p>
