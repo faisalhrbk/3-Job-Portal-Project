519579ed3183ca4ffc0f273e6602cf7f714bc2dc
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-12 16:35:04 +0500

    applied if statments

--- resources/views/jobDetail.blade.php
+++ resources/views/jobDetail.blade.php
@@ -1,107 +1,125 @@
 @extends('layouts.app')
 
 @section('main')
-   <section class="section-4 bg-2">    
-    <div class="container pt-5">
-        <div class="row">
-            <div class="col">
-                <nav aria-label="breadcrumb" class=" rounded-3 p-3">
-                    <ol class="breadcrumb mb-0">
-                        <li class="breadcrumb-item"><a href="{{ route('jobs') }}"><i class="fa fa-arrow-left" aria-hidden="true"></i> &nbsp;Back to Jobs</a></li>
-                    </ol>
-                </nav>
+    <section class="section-4 bg-2">
+        <div class="container pt-5">
+            <div class="row">
+                <div class="col">
+                    <nav aria-label="breadcrumb" class="rounded-3 p-3">
+                        <ol class="breadcrumb mb-0">
+                            <li class="breadcrumb-item"><a href="{{ route('jobs') }}"><i class="fa fa-arrow-left"
+                                        aria-hidden="true"></i> &nbsp;Back to Jobs</a></li>
+                        </ol>
+                    </nav>
+                </div>
             </div>
-        </div> 
-    </div>
-    <div class="container job_details_area">
-        <div class="row pb-5">
-            <div class="col-md-8">
-                <div class="card shadow border-0">
-                    <div class="job_details_header">
-                        <div class="single_jobs white-bg d-flex justify-content-between">
-                            <div class="jobs_left d-flex align-items-center">
-                                
-                                <div class="jobs_conetent">
-                                    <a href="#">
-                                        <h4>{{ $job->title }}</h4>
-                                    </a>
-                                    <div class="links_locat d-flex align-items-center">
-                                        <div class="location">
-                                            <p> <i class="fa fa-map-marker"></i>{{ $job->location }}</p>
-                                        </div>
-                                        <div class="location">
-                                            <p> <i class="fa fa-clock-o"></i> {{ $job->jobType->name }}</p>
+        </div>
+        <div class="job_details_area container">
+            <div class="row pb-5">
+                <div class="col-md-8">
+                    <div class="card border-0 shadow">
+                        <div class="job_details_header">
+                            <div class="single_jobs white-bg d-flex justify-content-between">
+                                <div class="jobs_left d-flex align-items-center">
+
+                                    <div class="jobs_conetent">
+                                        <a href="#">
+                                            <h4>{{ $job->title }}</h4>
+                                        </a>
+                                        <div class="links_locat d-flex align-items-center">
+                                            <div class="location">
+                                                <p> <i class="fa fa-map-marker"></i>{{ $job->location }}</p>
+                                            </div>
+                                            <div class="location">
+                                                <p> <i class="fa fa-clock-o"></i> {{ $job->jobType->name }}</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="jobs_right">
-                                <div class="apply_now">
-                                    <a class="heart_mark" href="#"> <i class="fa fa-heart-o" aria-hidden="true"></i></a>
+                                <div class="jobs_right">
+                                    <div class="apply_now">
+                                        <a class="heart_mark" href="#"> <i class="fa fa-heart-o"
+                                                aria-hidden="true"></i></a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="descript_wrap white-bg">
-                        <div class="single_wrap">
-                            <h4>Job description</h4>
-                           {!! nl2br($job->description ) !!}
-                        </div>
-                        <div class="single_wrap">
-                            <h4>Responsibility</h4>
-                        {!! nl2br($job->responsibility) !!}
-                        </div>
-                        <div class="single_wrap">
-                            <h4>Qualifications</h4>
-                          {!! nl2br($job->qualification) !!}
-                        </div>
-                        <div class="single_wrap">
-                            <h4>Benefits</h4>
-                          {!! nl2br($job->benefits) !!}
-                        </div>
-                        <div class="border-bottom"></div>
-                        <div class="pt-3 text-end">
-                            <a href="#" class="btn btn-secondary">Save</a>
-                            <a href="#" class="btn btn-primary">Apply</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card shadow border-0">
-                    <div class="job_sumary">
-                        <div class="summery_header pb-1 pt-4">
-                            <h3>Job Summery</h3>
-                        </div>
-                        <div class="job_content pt-3">
-                            <ul>
-                                <li>Published on: <span>{{ $job->created_at }}</span></li>
-                                <li>Vacancy: <span>2 Position</span></li>
-                                <li>Salary: <span>50k - 120k/y</span></li>
-                                <li>Location: <span>California, USA</span></li>
-                                <li>Job Nature: <span> Full-time</span></li>
-                            </ul>
+                        <div class="descript_wrap white-bg">
+                            <div class="single_wrap">
+                                <h4>Job description</h4>
+                                {!! nl2br($job->description) !!}
+                            </div>
+                            <div class="single_wrap">
+                                <h4>Responsibility</h4>
+                                @if (!empty($job->responsibility))
+                                    {!! nl2br($job->responsibility) !!}
+                                @endif
+
+                            </div>
+                            <div class="single_wrap">
+                                <h4>Qualifications</h4>
+                                @if (!empty($job->qualifications))
+                                    {!! nl2br($job->qualifications) !!}
+                                @endif
+                            </div>
+                            <div class="single_wrap">
+                                <h4>Benefits</h4>
+                                @if (!empty($job->benefits))
+                                    {!! nl2br($job->benefits) !!}
+                                @endif
+                            </div>
+                            <div class="border-bottom"></div>
+                            <div class="pt-3 text-end">
+                                <a href="#" class="btn btn-secondary">Save</a>
+                                <a href="#" class="btn btn-primary">Apply</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="card shadow border-0 my-4">
-                    <div class="job_sumary">
-                        <div class="summery_header pb-1 pt-4">
-                            <h3>Company Details</h3>
+                <div class="col-md-4">
+                    <div class="card border-0 shadow">
+                        <div class="job_sumary">
+                            <div class="summery_header pb-1 pt-4">
+                                <h3>Job Summery</h3>
+                            </div>
+                            <div class="job_content pt-3">
+                                <ul>
+                                    <li>Published on:
+                                        <span>{{ \Carbon\Carbon::parse($job->created_at)->format('d-M-Y') }}</span>
+                                    </li>
+                                    <li>Vacancy: <span>{{ $job->vacancy }}</span></li>
+                                    @if (!empty($job->salary))
+                                        <li>Salary: <span>{{ $job->salary }}</span></li>
+                                    @endif
+
+                                    <li>Location: <span>{{ $job->location }}</span></li>
+                                    <li>Job Nature: <span>{{ $job->jobType->name }}</span></li>
+                                </ul>
+                            </div>
                         </div>
-                        <div class="job_content pt-3">
-                            <ul>
-                                <li>Name: <span>XYZ Company</span></li>
-                                <li>Locaion: <span>Noida</span></li>
-                                <li>Webite: <span>www.example.com</span></li>
-                            </ul>
+                    </div>
+                    <div class="card my-4 border-0 shadow">
+                        <div class="job_sumary">
+                            <div class="summery_header pb-1 pt-4">
+                                <h3>Company Details</h3>
+                            </div>
+                            <div class="job_content pt-3">
+                                <ul>
+                                    <li>Name: <span>{{ $job->company_name }}</span></li>
+                                    @if (!empty($job->salary))
+                                        <li>Location: <span>{{ $job->location }}</span></li>
+                                    @endif
+                                    @if (!empty($job->company_website))
+                                        <span><a href="{{ $job->company_website }}"></a>{{ $job->company_website }}</span>
+                                    @endif
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
 
 @section('customJs')
